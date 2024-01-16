98f1b6b0dc44ab8efabde4cd5b120db525d70fa5
Gh-Yeh <yehyaghady@gmail.com>
2024-01-16 22:39:59 +0200

    tasks

---
+++ 2-selection_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Function that sorts an array using selection sort
+ * @array: Array to be sorted
+ * @size: Size of array given
+ * Return: 0
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
+		{
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
+	}
+}
+/**
+ * swap - to swap values
+ * @vl_one: Fisrt value
+ * @vl_two: Second value
+ * Return: 0
+ */
+void swap(int *vl_one, int *vl_two)
+{
+	int temp;
+
+	temp = *vl_two;
+	*vl_two = *vl_one;
+	*vl_one = temp;
+}
